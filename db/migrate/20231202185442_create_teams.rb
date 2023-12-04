fd032a044be96c687c0c85288309666c5b04d8d4
aronianm <aronian.m@gmail.com>
2023-12-03 20:22:17 -0500

    Sign Up
    App can signup user with JWT token

--- db/migrate/20231202185442_create_teams.rb
+++ db/migrate/20231202185442_create_teams.rb
@@ -2,7 +2,7 @@ class CreateTeams < ActiveRecord::Migration[7.0]
   def change
     create_table :teams do |t|
       t.string :name
-      t.authentification_code :string
+      t.string :authentification_code, unique: true
       t.integer :admin_user_id
       t.timestamps
     end
