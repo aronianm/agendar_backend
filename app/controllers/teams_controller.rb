55f9030292b8dab35860b6c11e0f7db8bdae3253
aronianm <aronian.m@gmail.com>
2023-12-05 20:30:07 -0500

    authenticate user

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,46 @@
 class TeamsController < ApplicationController
 
     def index
+        render :json => Team.where(:admin_user_id => current_user.id)
+    end
+
+    def create
+        team = Team.new(teams_params)
+        team.admin_user_id = current_user.id
+        if team.save
+            render json: {
+                status: 200,
+                data: current_user,
+                message: 'Succesfully created team'
+            }, status: :ok
+        else
+            render json: {
+                status: 500,
+                message: "Could not save"
+            }, status: :error
+        end
+    end
+
+    def destroy
+        team = Team.find(params[:id])
+        if team.delete
+            render json: {
+                status: 200,
+                data: current_user,
+                message: 'Succesfully deleted team'
+            }, status: :ok
+        else
+            render json: {
+                status: 500,
+                message: "Could not delete team"
+            }, status: :error
+        end
+    end
+
+
+    private
+    def teams_params
+        params.require('teams').permit(:name)
     end
 
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,12 @@
 class Team < ApplicationRecord
+
+    before_create :generate_access_code
+
+
+
+
+    private
+    def generate_access_code
+        self.authentification_code = "#{self.admin_user_id}-#{rand(10 ** 10).to_s.rjust(10,'0')}"
+    end
 end
